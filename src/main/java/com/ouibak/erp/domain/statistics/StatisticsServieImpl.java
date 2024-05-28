7aac745eab58603d8f8e62c64a45d816af2d1b7b
이연수 <dldustn2040@gmail.com>
2024-05-28 15:01:02 +0900

    fix: conflict merge

--- src/main/java/com/ouibak/erp/domain/statistics/StatisticsServieImpl.java
+++ src/main/java/com/ouibak/erp/domain/statistics/StatisticsServieImpl.java
@@ -1,9 +1,6 @@
 package main.java.com.ouibak.erp.domain.statistics;
 
-import main.java.com.ouibak.erp.dao.Query;
-
 import java.sql.SQLException;
-import java.util.Arrays;
 import java.util.List;
 
 public class StatisticsServieImpl implements StatisticsService{
@@ -21,18 +18,7 @@ public class StatisticsServieImpl implements StatisticsService{
     }
 
     @Override
-    public void getProductList() throws SQLException {
-        String[] arr = dao.getAllProduct(Query.getQuery("getProductListS"));
-        System.out.println(Arrays.toString(arr));
-        StatisticsVO.getInstance().setProductArr(arr);
-//        new StatisticsVO().geti.setProductArr(arr);
-
-//        return arr;
-    }
-
-    @Override
     public List<List<Integer>> getStatistics(String productName) throws SQLException {
-//        StatisticsVO.getInstance().setStatistics(dao.getStatistics("쿼리", productName));
         return dao.getStatistics("", productName);
     }
 
