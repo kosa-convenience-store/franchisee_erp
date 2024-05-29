659b9a0ce115a78d45fd383ff10e400fd9e8cb54
Wildturkeyy <109601078+Wildturkeyy@users.noreply.github.com>
2024-05-29 17:44:06 +0900

    FEAT: 버튼 클릭시 발주현황 반영 추가

--- src/main/java/com/ouibak/erp/gui/tabpannel/orderGui/OrderTabGui.java
+++ src/main/java/com/ouibak/erp/gui/tabpannel/orderGui/OrderTabGui.java
@@ -1,3 +1,37 @@
+//package main.java.com.ouibak.erp.gui.tabpannel.orderGui;
+//
+//import javax.swing.*;
+//import java.awt.*;
+//
+//public class OrderTabGui extends JFrame {
+//    private JPanel cardPanel;
+//    private CardLayout cardLayout;
+//
+//    public JPanel createOrderManagementPanel() {
+//        JPanel panel = new JPanel(new BorderLayout());
+//
+//        JPanel sidePanel = new JPanel(new GridLayout(2, 1));
+//        JButton registerButton = new JButton("발주 등록");
+//        JButton historyButton = new JButton("발주 내역");
+//        sidePanel.add(registerButton);
+//        sidePanel.add(historyButton);
+//
+//        cardLayout = new CardLayout();
+//        cardPanel = new JPanel(cardLayout);
+//        cardPanel.add(new OrderGui().createOrderRegistrationPanel(), "발주 등록");
+//        cardPanel.add(new OrderListGui().createOrderHistoryPanel(), "발주 내역");
+//
+//        panel.add(sidePanel, BorderLayout.WEST);
+//        panel.add(cardPanel, BorderLayout.CENTER);
+//
+//        registerButton.addActionListener(e -> cardLayout.show(cardPanel, "발주 등록"));
+//        historyButton.addActionListener(e -> cardLayout.show(cardPanel, "발주 내역"));
+//
+//        return panel;
+//    }
+//
+//}
+
 package main.java.com.ouibak.erp.gui.tabpannel.orderGui;
 
 import javax.swing.*;
@@ -6,6 +40,11 @@ import java.awt.*;
 public class OrderTabGui extends JFrame {
     private JPanel cardPanel;
     private CardLayout cardLayout;
+    private OrderListGui orderListGui;
+
+    public OrderListGui getOrderListGui() {
+        return orderListGui;
+    }
 
     public JPanel createOrderManagementPanel() {
         JPanel panel = new JPanel(new BorderLayout());
@@ -19,13 +58,17 @@ public class OrderTabGui extends JFrame {
         cardLayout = new CardLayout();
         cardPanel = new JPanel(cardLayout);
         cardPanel.add(new OrderGui().createOrderRegistrationPanel(), "발주 등록");
-        cardPanel.add(new OrderListGui().createOrderHistoryPanel(), "발주 내역");
+        orderListGui = new OrderListGui();
+        cardPanel.add(orderListGui.createOrderHistoryPanel(), "발주 내역");
 
         panel.add(sidePanel, BorderLayout.WEST);
         panel.add(cardPanel, BorderLayout.CENTER);
 
         registerButton.addActionListener(e -> cardLayout.show(cardPanel, "발주 등록"));
-        historyButton.addActionListener(e -> cardLayout.show(cardPanel, "발주 내역"));
+        historyButton.addActionListener(e -> {
+            orderListGui.resetTableData();  // 발주 내역 갱신
+            cardLayout.show(cardPanel, "발주 내역");
+        });
 
         return panel;
     }
